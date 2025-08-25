a9fb7f3fb9d6e43128cea9a8e663becd4a70a389
ben <ben76020567@gmail.com>
2025-08-25 15:12:43 +0800

    improve and fix picker

--- lua/proot/tele.lua
+++ lua/proot/tele.lua
@@ -5,87 +5,91 @@ local actions = require("telescope.actions")
 local action_state = require("telescope.actions.state")
 local theme = require("telescope.themes")
 local detector = require("proot.detector")
+local conf = require("telescope.config").values
 
-local picker
 local _events
 
 local M = {}
 
-local make_displayer = function(entry)
-  return entry_display.create({
+local function create_finder()
+  local displayer = entry_display.create({
     separator = " ",
     items = {
-      { width = 20 },
-      { remaining = true },
+      {
+        width = 20,
+      },
+      {
+        remaining = true,
+      },
     },
-  })({ entry.name, { entry.value, "Comment" } })
-end
+  })
 
-local entry_maker = function(entry)
-  return {
-    name = entry.name,
-    value = entry.path,
-    display = make_displayer,
-    ordinal = 1,
-  }
+  local function make_display(entry)
+    return displayer({ entry.name, { entry.value, "Comment" } })
+  end
+
+  return finders.new_table({
+    results = detector.get_projects(),
+    entry_maker = function(entry)
+      return {
+        display = make_display,
+        name = entry.name,
+        value = entry.path,
+        ordinal = entry.name,
+      }
+    end,
+  })
 end
 
 local new_picker = function()
-  if picker then
-    pcall(picker.delete)
-    picker = nil
-  end
-
-  local projects = detector.get_projects()
-  picker = pickers.new(theme.get_dropdown(), {
-    prompt_title = "Proot",
-    finder = finders.new_table({
-      results = projects,
-      entry_maker = entry_maker,
-    }),
-    attach_mappings = function(_, map)
-      actions.select_default:replace(function(prompt_bufnr)
-        local selection = action_state.get_selected_entry()
-        actions.close(prompt_bufnr)
-        vim.fn.chdir(selection.value)
-        if _events.entered then
-          _events.entered(selection.value)
-        end
-        detector.move_project_to_top(selection.value)
-      end)
-      map("n", "d", M.delete_project)
-      map("n", "r", M.rename_project)
-      return true
-    end,
-  })
-  picker:find()
+  pickers
+    .new(theme.get_dropdown(), {
+      prompt_title = "Proot",
+      finder = create_finder(),
+      sorter = conf.generic_sorter({}),
+      attach_mappings = function(_, map)
+        actions.select_default:replace(function(prompt_bufnr)
+          local selection = action_state.get_selected_entry()
+          actions.close(prompt_bufnr)
+          vim.fn.chdir(selection.value)
+          if _events.entered then
+            _events.entered(selection.value)
+          end
+          detector.move_project_to_top(selection.value)
+        end)
+        map("n", "d", M.delete_project)
+        map("n", "r", M.rename_project)
+        return true
+      end,
+    })
+    :find()
 end
 
-local refresh_picker = function()
-  picker:refresh(finders.new_table({
-    results = detector.get_projects(),
-    entry_maker = entry_maker,
-  }))
+local refresh_picker = function(prompt_bufnr)
+  local finder = create_finder()
+  action_state.get_current_picker(prompt_bufnr):refresh(finder, {
+    reset_prompt = true,
+  })
 end
 
 M.init = function(events)
   _events = events
 end
 
-M.delete_project = function()
+M.delete_project = function(prompt_bufnr)
   local projects = detector.get_projects()
   local path = action_state.get_selected_entry().value
   for i, project in ipairs(projects) do
     if project.path == path then
       table.remove(projects, i)
       detector.set_projects(projects)
-      refresh_picker()
+      refresh_picker(prompt_bufnr)
       return
     end
   end
 end
 
-M.rename_project = function()
+M.rename_project = function(prompt_bufnr)
   local selected_entry = action_state.get_selected_entry()
   local input = vim.fn.input("Rename project: ", selected_entry.name)
 
@@ -98,7 +102,7 @@ M.rename_project = function()
     if project.path == selected_entry.value then
       project.name = input
       detector.set_projects(projects)
-      refresh_picker()
+      refresh_picker(prompt_bufnr)
       return
     end
   end
